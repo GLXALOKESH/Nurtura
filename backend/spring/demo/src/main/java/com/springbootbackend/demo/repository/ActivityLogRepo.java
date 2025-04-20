956404820b55e2de6672195de8edba720efdb23b
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-04-20 10:44:34 +0530

    activity log repo

--- backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/ActivityLogRepo.java
+++ backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/ActivityLogRepo.java
@@ -4,6 +4,9 @@ import com.springbootbackend.demo.model.ActivityLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityLogRepo extends MongoRepository<ActivityLog , String> {
+    List<ActivityLog> findByUserId_Id(String userId);
 }
